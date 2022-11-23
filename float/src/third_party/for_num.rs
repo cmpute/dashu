416e075537e070bb289e9aad138e192fc3045a9a
Aster <galaster@foxmail.com>
2022-11-23 18:14:15 +0800

    Add support for `FromPrimitive`

--- float/src/third_party/for_num.rs
+++ float/src/third_party/for_num.rs
@@ -1,4 +1,4 @@
-use num_traits::{One, Zero};
+use num_traits::{FromPrimitive, One, ToPrimitive, Zero};
 
 use dashu_int::Word;
 
@@ -31,8 +31,70 @@ impl<R: Round, const B: Word> One for FBig<R, B> {
     }
 }
 
+impl<R: Round, const B: Word> FromPrimitive for FBig<R, B> {
+    #[inline]
+    fn from_isize(n: isize) -> Option<Self> {
+        Some(FBig::from(n))
+    }
+    #[inline]
+    fn from_i8(n: i8) -> Option<Self> {
+        Some(FBig::from(n))
+    }
+    #[inline]
+    fn from_i16(n: i16) -> Option<Self> {
+        Some(FBig::from(n))
+    }
+    #[inline]
+    fn from_i32(n: i32) -> Option<Self> {
+        Some(FBig::from(n))
+    }
+    #[inline]
+    fn from_i64(n: i64) -> Option<Self> {
+        Some(FBig::from(n))
+    }
+    #[inline]
+    fn from_i128(n: i128) -> Option<Self> {
+        Some(FBig::from(n))
+    }
+    #[inline]
+    fn from_usize(n: usize) -> Option<Self> {
+        Some(FBig::from(n))
+    }
+    #[inline]
+    fn from_u8(n: u8) -> Option<Self> {
+        Some(FBig::from(n))
+    }
+    #[inline]
+    fn from_u16(n: u16) -> Option<Self> {
+        Some(FBig::from(n))
+    }
+    #[inline]
+    fn from_u32(n: u32) -> Option<Self> {
+        Some(FBig::from(n))
+    }
+    #[inline]
+    fn from_u64(n: u64) -> Option<Self> {
+        Some(FBig::from(n))
+    }
+    #[inline]
+    fn from_u128(n: u128) -> Option<Self> {
+        Some(FBig::from(n))
+    }
+    // #[inline]
+    // fn from_f32(n: f32) -> Option<Self> {
+    //     FBig::to_f32()
+    // }
+    // #[inline]
+    // fn from_f64(n: f64) -> Option<Self> {
+    //     FBig::try_from(n).ok()
+    // }
+}
+
+
+
 #[cfg(test)]
 mod tests {
+    use std::ops::Neg;
     use crate::DBig;
 
     use super::*;
@@ -46,5 +108,11 @@ mod tests {
         assert!(!DBig::from(0).is_one());
         assert!(!DBig::from(1).is_zero());
         assert!(DBig::from(1).is_one());
+    }
+
+    #[test]
+    fn test_from() {
+        assert_eq!(DBig::from_usize(1), Some(DBig::one()));
+        assert_eq!(DBig::from_isize(-1), Some(DBig::one().neg()));
     }
 }
